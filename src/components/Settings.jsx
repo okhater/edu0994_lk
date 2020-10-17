2f9f70b5f69eb8ac82e14154ee502c5d457563aa
Olga <aquapura87@gmail.com>
2020-10-17 21:41:56 +0300

    Урок 15.10.2020

--- src/components/Settings.jsx
+++ src/components/Settings.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 
-export const Settings = ()=>{
-    return <div className="row">
-        <div className="container my-5">
-            <p> Имя: <span> Иван </span> </p>
-            <p> Фамилия: <span> Иванов </span> </p>
-            <p> Email: <span> ivanov@mail.ru </span> </p>
-            <p>ID: <span> 1 </span> </p>
+export class Settings extends React.Component{
+    render(){
+        return <div className="row">
+            <div className="container my-5">
+                <p> Имя: <span> Иван </span> </p>
+                <p> Фамилия: <span> Иванов </span> </p>
+                <p> Email: <span> ivanov@mail.ru </span> </p>
+                <p>ID: <span> 1 </span> </p>
+            </div>
         </div>
-    </div>
+    }
 }
--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,38 +1,40 @@
 import React from "react";
 
-export const Users = ()=>{
-    return <table className="table table-striped">
-        <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Имя</th>
-            <th scope="col">Фамилия</th>
-            <th scope="col">email</th>
-            <th scope="col">id</th>
-        </tr>
-        </thead>
-        <tbody>
-        <tr>
-            <th scope="row">1</th>
-            <td>Ivan</td>
-            <td>Ivanov</td>
-            <td>ivanov@mail.ru</td>
-            <td>id=1</td>
-        </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>Thornton@mail.ru</td>
-            <td>id=2</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>theBird@mail.ru</td>
-            <td>id=3</td>
-        </tr>
-        </tbody>
-    </table>
+export class Users extends React.Component{
+    render(){
+        return <table className="table table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Имя</th>
+                <th scope="col">Фамилия</th>
+                <th scope="col">email</th>
+                <th scope="col">id</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>Ivan</td>
+                <td>Ivanov</td>
+                <td>ivanov@mail.ru</td>
+                <td>id=1</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>Thornton@mail.ru</td>
+                <td>id=2</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>Larry</td>
+                <td>the Bird</td>
+                <td>theBird@mail.ru</td>
+                <td>id=3</td>
+            </tr>
+            </tbody>
+        </table>
+    }
 }
